54aa52f532c5242a9a4939109edea8c2892612a4
imo-tikuwa <imotikuwabathclean@gmail.com>
2019-12-30 16:14:33 +0900

    thinreportsのサンプル

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'thinreports'
+require 'date'
+
+# PDFをダウンロードするルーティング
+get '/download_pdf' do
+
+  test1 = Time.now.strftime("%Y年%m月%d日 %H時%M分%S秒")
+  file_name = Time.now.strftime("%Y%m%d%H%M%S") + '.pdf'
+  file_path = 'output/' + file_name
+
+  # PDF生成
+  Thinreports::Report.generate(:filename => file_path, :layout => 'input/example.tlf') do
+    start_new_page
+    page.item(:test1).value(test1)
+  end
+
+  # PDFダウンロード
+  stat = File::stat(file_path)
+  send_file(file_path, :filename => file_name, :length => stat.size, :type => 'application/octet-stream')
+end
